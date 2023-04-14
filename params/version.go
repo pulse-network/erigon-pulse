ee67a0013de39922736a5576541624ec85abd355
Shane Bammel <sjb933@gmail.com>
2023-04-14 12:52:40 -0500

    Prepare stable release

--- params/version.go
+++ params/version.go
@@ -31,9 +31,9 @@ var (
 
 // see https://calver.org
 const (
-	VersionMajor       = 1        // Major version component of the current release
+	VersionMajor       = 2        // Major version component of the current release
 	VersionMinor       = 0        // Minor version component of the current release
-	VersionMicro       = 2        // Patch version component of the current release
+	VersionMicro       = 0        // Patch version component of the current release
 	VersionModifier    = "stable" // Modifier component of the current release
 	VariantMeta        = "pulse"  // Variant metadata to append to the version string
 	VersionKeyCreated  = "ErigonVersionCreated"
